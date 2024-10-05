4f8574c836eb643fc62a6ee4658e7122d1c9c40d
Witold Pazderski <witold.pazderski@gmail.com>
2024-10-05 07:06:02 +0200

    Remove unused types

--- src/i18n/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Opaque } from "../types";
-
-declare const _TimeZone: unique symbol;
-export type TimeZone = Opaque<string, typeof _TimeZone>;
-
-export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
